bb09a3d72716af1cb9114cb6ce4f0643f1b74485
Ajith Nambiar <ajith.n@tataelxsi.co.in>
2021-11-16 08:54:38 +0530

    RDKSI-6059: VPN passthrough for PPTP is allowing IPv6 traffic even after disabling PPTP DM
    Reason for change: Added rule for tcp protocol instead of udp for PPTP
    Test Procedure: Test results are captured in the ticket - RDKSI-6059.
    Risks: None
    
    Signed-off-by: Ajith Nambiar <ajith.n@tataelxsi.co.in>

--- source/firewall/firewall.c
+++ source/firewall/firewall.c
@@ -15053,10 +15053,10 @@ v6GPFirewallRuleNext:
     queryv6[0] = '\0';
 
     if((0==syscfg_get(NULL, "blockpptp::result", queryv6, sizeof(queryv6))) && strcmp(queryv6,"$ACCEPT") == 0){
-        fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 1723  -j ACCEPT\n");
+        fprintf(fp, "-A lan2wan_misc_ipv6 -p tcp --dport 1723  -j ACCEPT\n");
     }
     else {
-        fprintf(fp, "-A lan2wan_misc_ipv6 -p udp --dport 1723  -j DROP\n");
+        fprintf(fp, "-A lan2wan_misc_ipv6 -p tcp --dport 1723  -j DROP\n");
     }
     fprintf(fp, "-I lan2wan -j lan2wan_misc_ipv6\n");
 #endif
