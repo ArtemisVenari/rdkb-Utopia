ArtemisVenari/rdkb-Utopia

commit 5b938195615a49d235f1c49699d137bdafbac255

RDKGWS-140: enable the wan mode of PHY2.5G port for smart5typa
we enable the WAN mode of PHY2.5G port aka. blue-ethernet-port for
the hardware smart5typa.

Signed-off-by: Xiaobo Sun <xiaobo.sun@telekom.de>

source/scripts/init/system/utopia_init.sh
@@ -346,7 +346,7 @@ fi
 
 # model check and ethswctl commands execution
 model=`grep "imagename" /version.txt | awk -F'_v' '{print $1}' | awk -F':' '{print $2}'`
-if [ "$model" == "DGA4333" -o "$model" = "FAST5386" ]; then
+if [ "$model" == "DGA4333" -o "$model" = "FAST5386" -o "$model" = "SMART5TYPA"]; then
     ethswctl -c wan -i eth0 -o enable
 elif [ "$model" == "FGA2233" -o "$model" = "FAST5670DT" ]; then
     ethswctl -c softswitch -i eth4 -o enable

commit 72f370e9ab7e46006440da4231ee6346a7b9edab

RDKGWS-230: fix shell to enable WAN
there is a bug "no space before close square-bracket" introduced
by the previous commit 5b938195.

Signed-off-by: Xiaobo Sun <xiaobo.sun@telekom.de>
Change-Id: Ie26f722f81218c8bd89cb5b5246da6419ac2bf8e

source/scripts/init/system/utopia_init.sh
@@ -92,7 +92,7 @@ cat /proc/P-UNIT/status
 echo_t "*                                                                  "
 echo_t "*******************************************************************"
 
-echo "[utopia][init] Tweaking network parameters" > /dev/console
+echo_t "[utopia][init] Tweaking network parameters" > /dev/console
 
 KERNEL_VERSION=`uname -r | cut -c 1`
 
@@ -346,7 +346,8 @@ fi
 
 # model check and ethswctl commands execution
 model=`grep "imagename" /version.txt | awk -F'_v' '{print $1}' | awk -F':' '{print $2}'`
-if [ "$model" == "DGA4333" -o "$model" = "FAST5386" -o "$model" = "SMART5TYPA"]; then
+echo_t "utopia_init.sh model: $model"
+if [ "$model" == "DGA4333" -o "$model" = "FAST5386" -o "$model" = "SMART5TYPA" ]; then
     ethswctl -c wan -i eth0 -o enable
 elif [ "$model" == "FGA2233" -o "$model" = "FAST5670DT" ]; then
     ethswctl -c softswitch -i eth4 -o enable